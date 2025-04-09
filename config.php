319909d2cfb84c4c28a7b57b758b1e69fe75b4c4
DESKTOP-4T1N182\LENOVO <khminhcanfly@gmail.com>
2025-04-09 15:43:33 +0700

    nháp

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
-$host = "localhost";  // XAMPP default
-$user = "root";       // Default user in XAMPP (no password by default)
-$password = "";       // No password in default XAMPP setup
-$database = "cv"; // Your database name
 
-// Create connection
-$conn = new mysqli($host, $user, $password, $database);
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "cv";
+$db_port = 3307;
 
-// Check connection
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $db_port);
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
--- header.php
+++ header.php
@@ -13,22 +13,22 @@
             <ul id="navbar" class="nav-links">
                 <li class="lists"><a class="options" href="index.php">Home</a></li>
                 <li class="lists">
-                    <select class="cv-dropdown" onchange="if (this.value) window.location.href = this.value;">
-                        <option value="" disabled selected>CV</option>
-                        <option value="demo.php">My CV</option>
-                        <option value="Update_CV.php">Update CV</option>
-                        <option value="create_cv.php">Create New CV</option>
-                    </select>
-                </li>
+    <div class="dropdown-wrapper">
+        <select class="cv-dropdown" onchange="if (this.value) window.location.href = this.value;">
+            <option value="" disabled selected>CV</option>
+            <option value="demo.php">My CV</option>
+            <option value="Update_CV.php">Update CV</option>
+            <option value="create_cv.php">Create New CV</option>
+        </select>
+    </div>
+</li>
                 <li class="lists"><a class="options" href="template.php">Templates</a></li>
-                <li class="lists"><a class="options" href="#">Contact</a></li>
-                <li id="log_list">
+                <li class="lists"><a class="options" href="contact.php">Contact</a></li>
+                <li class="lists" id="log_list">
                     <?php if (isset($_SESSION['username'])): ?>
-                        <a id="log_option" href="logout.php" class="btn">
-                            <?php echo htmlspecialchars($_SESSION['username']) . " Logout"; ?>
-                        </a>
+                        <a id="log_option" href="logout.php" class="btn options">Log out</a>
                     <?php else: ?>
-                        <a id="log_option" href="logout.php" class="btn">Login</a>
+                        <a id="log_option" href="logout.php" class="btn options">Login</a>
                     <?php endif; ?>
                 </li>
             </ul>

179e0736f64f6feb6fa4ad31a9e29a2ecab0a3e2
DESKTOP-TSQE45N\tiend <dat.tran0912@hcmut.edu.vn>
2025-04-09 15:41:19 +0700

    hehe

---
+++ temp/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Creation and Hosting</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header style="position: fixed; width: 100%; top: 0; z-index: 1000;">
+        <nav class="navbar">
+            <div class="logo"><a href="index.php">CV</a></div>
+            <ul id="navbar" class="nav-links">
+                <li class="lists"><a href="index.php">Home</a></li>
+                <li id="cv" class="lists"><a>CV Management</a>
+                    <ul class="dropdown">
+                        <li class="lists"><a href="demo.php">My CV</a></li>
+                        <li class="lists"><a href="Update_CV.php">Update CV</a></li>
+                        <li class="lists"><a href="create_cv.php">Create New CV</a></li>
+                    </ul>
+                </li>
+                <li class="lists"><a href="template.php">Templates</a></li>
+                <li class="lists"><a href="#">Contact</a></li>
+                <li id="log_list">
+                    
+                </li>
+            </ul>
+            <button class="menu-toggle">&#9776;</button>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ temp/home.php
@@ -0,0 +1,58 @@
+
+<?php
+include_once("login_db.php");
+
+if(isset($_SESSION['username'])){
+	header("Location:login.php");
+}
+?>
+<?php @include 'header.php'; ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main class="main-content">
+        <section class="home" style="background-image: url('images/contact.jpg'); background-size: cover;">
+            <div class="container">
+                <h1>Our wweb</h1>
+                <p>Cham ngon.</p>
+                <a href="template.php" class="btn">Create my CV</a>
+            </div>
+        </section>
+        <section class="features">
+            <div class="container">
+                <h2>Features</h2>
+            <div class="feature-row" style="display: flex; gap: 80px;">
+                <div class="feature-item">
+                    <i class="fas fa-pencil-alt"></i>
+                    <h3>Easy to Use</h3>
+                    <p>Create your CV with our user-friendly interface.</p>
+                </div>
+                <div class="feature-item">
+                    <i class="fas fa-cloud-upload-alt"></i>
+                    <h3>Host Online</h3>
+                    <p>Host your CV online and share it with a unique link.</p>
+                </div>
+                <div class="feature-item">
+                    <i class="fas fa-file-alt"></i>
+                    <h3>Multiple Templates</h3>
+                    <p>Choose from a variety of professional templates.</p>
+                </div>
+            </div>
+            </div>
+        </section>
+    </div></main>
+
+    <?php @include 'footer.php'; ?>
+</body>
+</html>

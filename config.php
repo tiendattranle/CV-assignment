f46752ad5cd02edf82f06a2577764be796c7196b
DESKTOP-4T1N182\LENOVO <khminhcanfly@gmail.com>
2025-04-09 18:00:44 +0700

    no message

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ $host = "localhost";  // XAMPP default
 $user = "root";       // Default user in XAMPP (no password by default)
 $password = "";       // No password in default XAMPP setup
 $database = "cv"; // Your database name
+$db_port = 3307;
 
 // Create connection
-$conn = new mysqli($host, $user, $password, $database);
-
+$conn = new mysqli($host, $user, $password, $database, $db_port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
